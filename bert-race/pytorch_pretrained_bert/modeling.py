daaae548cf7d5b557a11a8028371c42a4914652d
Matteo Karl Donati <donati.matteo.k@gmail.com>
2020-05-28 17:12:41 +0200

    small changes

--- bert-race/pytorch_pretrained_bert/modeling.py
+++ bert-race/pytorch_pretrained_bert/modeling.py
@@ -45,7 +45,7 @@ PRETRAINED_MODEL_ARCHIVE_MAP = {
     'bert-base-multilingual-cased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-cased.tar.gz",
     'bert-base-chinese': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-chinese.tar.gz",
 }
-CONFIG_NAME = 'bert_config.json'
+CONFIG_NAME = 'config.json'
 WEIGHTS_NAME = 'pytorch_model.bin'
 
 def gelu(x):
