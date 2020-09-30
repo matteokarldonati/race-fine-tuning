2dedea47bcc5879e0cd318f0c1f0a34628af410e
Matteo Karl Donati <donati.matteo.k@gmail.com>
2020-09-30 17:12:54 +0200

    qsub scripts

---
+++ qsub_scripts/albert_xxlarge.sh
@@ -0,0 +1,34 @@
+#$ -S /bin/bash
+
+#$ -N ALBERT_XXLARGE
+#$ -j y
+
+#$ -l tmem=16G
+#$ -l h_rt=72:00:00
+#$ -l gpu=true
+#$ -pe gpu 2
+#$ -R y
+
+#$ -cwd
+
+export PATH=/share/apps/python-3.7.2-shared/bin:${PATH}
+export LD_LIBRARY_PATH=/share/apps/python-3.7.2-shared/lib:${LD_LIBRARY_PATH}
+
+source /share/apps/examples/source_files/cuda/cuda-10.1.source
+
+export RACE_DIR=../../RACE
+python ../transformers-examples/run_multiple_choice.py \
+--task_name race \
+--model_name_or_path albert-xxlarge-v2 \
+--do_train \
+--do_eval \
+--do_predict \
+--data_dir $RACE_DIR \
+--learning_rate 1e-5 \
+--num_train_epochs 4 \
+--max_seq_length 640 \
+--output_dir ../output/albert_xxlarge-race \
+--per_gpu_eval_batch_size=2 \
+--per_gpu_train_batch_size=2 \
+--gradient_accumulation_steps 16 \
+--overwrite_output \
---
+++ qsub_scripts/bert_large.sh
@@ -0,0 +1,31 @@
+#$ -S /bin/bash
+
+#$ -N BERT_LARGE
+#$ -j y
+
+#$ -l tmem=16G
+#$ -l h_rt=100:00:00
+#$ -l gpu=true,gpu_v100=yes
+# $ -pe gpu 2
+#$ -R y
+
+#$ -cwd
+
+export PATH=/share/apps/python-3.7.2-shared/bin:${PATH}
+export LD_LIBRARY_PATH=/share/apps/python-3.7.2-shared/lib:${LD_LIBRARY_PATH}
+
+source /share/apps/examples/source_files/cuda/cuda-10.1.source
+
+python ../bert-race/run_race.py \
+--data_dir=RACE \
+--bert_model=bert-large-uncased \
+--output_dir=../output/bert_large-race \
+--max_seq_length=320 \
+--do_train \
+--do_eval \
+--do_lower_case \
+--train_batch_size=8 \
+--eval_batch_size=1 \
+--learning_rate=1e-5 \
+--num_train_epochs=2 \
+--gradient_accumulation_steps=8
